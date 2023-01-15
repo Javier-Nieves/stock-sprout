36d7e614c044c04b5d908d4d453ea62b223985d4
Konstantin Nikolskiy <kin310@yandex.ru>
2023-01-15 00:17:06 -0500

    build.sh script added

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
