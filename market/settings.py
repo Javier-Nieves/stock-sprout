7f7455eacf8f2a971069fd314d86ea0ee5bc41a7
Konstantin Nikolskiy <kin310@yandex.ru>
2023-01-14 23:20:20 -0500

    apps update

--- market/settings.py
+++ market/settings.py
@@ -29,7 +29,7 @@ AUTH_USER_MODEL = 'stocks.user'
 
 INSTALLED_APPS = [
     'stocks',
-    'mathfilters',
+    # 'mathfilters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
